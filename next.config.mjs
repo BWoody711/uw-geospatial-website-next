4b1d2a13407957410fb360869d488bad35c852dd
Ben Woodward <66846892+BWoody711@users.noreply.github.com>
2024-10-06 19:25:21 -0400

    Update config file for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // basePath: "/uw-geospatial-website-next",
-    // output: "export",  // <=== enables static exports
-    // reactStrictMode: true,
+    basePath: "/uw-geospatial-website-next",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
 };
 
 export default nextConfig;
